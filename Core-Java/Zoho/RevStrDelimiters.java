5c8bffa9585816c5e681a6197f7315eceb945066
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2023-09-28 21:21:45 +0530

    Updates

---
+++ Core-Java/Zoho/RevStrDelimiters.java
@@ -0,0 +1,42 @@
+package Zoho;
+
+import java.util.Scanner;
+
+public class RevStrDelimiters {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = "A man, in the boat says : I see 1-2-3 in the sky";
+        // String input = sc.nextLine();
+        char[] chars = input.toCharArray();
+        reverseStr(chars);
+        System.out.println(new String(chars));
+        sc.close();
+    }
+
+    public static void reverseStr(char[] chars) {
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            // Find the next non-alphabet character from the start
+            while (start < end && !Character.isLetterOrDigit(chars[start])) {
+                start++;
+            }
+
+            // Find the next non-alphabet character from the end
+            while (start < end && !Character.isLetterOrDigit(chars[end])) {
+                end--;
+            }
+
+            // Swap the characters at start and end positions
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+
+            // Move to the next positions
+            start++;
+            end--;
+        }
+    }
+}
--- Core-Java/Zoho/ZohoPatterns.java
+++ Core-Java/Zoho/ZohoPatterns.java
@@ -13,8 +13,20 @@ public class ZohoPatterns {
             System.out.println();
         }
     }
+    static void pattern_2(int n){
+        for(int row = 0; row < 2*n-1; row++){
+            for(int col = 0; col < 2*n-1; col++){
+                int top = row;
+                int bottom = col;
+                int right = (2*n-2)-col;
+                int left = (2*n-2)-row;
+                System.out.print(n-Math.min(Math.min(top,bottom),Math.min(right, left)) + " ");
+            }
+            System.out.println();
+        }
+    }
     
     public static void main(String[] args) {
-        pattern_1(6);
+        pattern_2(4);
     }
 }
