e81c89523ce3aa6dedd690928b6dc99bf8e62f4d
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2023-10-07 11:28:38 +0530

    updates

---
+++ Core-Java/Zoho/Round2/Strings/AnagramFinder.java
@@ -0,0 +1,34 @@
+package Zoho.Round2.Strings;
+import java.util.*;
+
+public class AnagramFinder {
+    public static void main(String[] args) {
+        String[] inputArray = {"catch", "got", "tiger", "mat", "eat", "Pat", "tap", "tea"};
+        String wordToFind = "ate";
+
+        List<String> result = findAnagrams(inputArray, wordToFind);
+        System.out.println("Anagrams of " + wordToFind + ": " + result);
+    }
+
+    public static List<String> findAnagrams(String[] inputArray, String wordToFind) {
+        List<String> anagrams = new ArrayList<>();
+
+        // Convert the word to find to lowercase and sort its characters
+        char[] sortedWordToFind = wordToFind.toLowerCase().toCharArray();
+        Arrays.sort(sortedWordToFind);
+
+        for (String word : inputArray) {
+            // Convert the current word to lowercase and sort its characters
+            char[] sortedWord = word.toLowerCase().toCharArray();
+            Arrays.sort(sortedWord);
+
+            // Compare the sorted words to find anagrams
+            if (Arrays.equals(sortedWord, sortedWordToFind)) {
+                anagrams.add(word);
+            }
+        }
+
+        return anagrams;
+    }
+}
+
