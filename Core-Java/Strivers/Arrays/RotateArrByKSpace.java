3398e1e6228816f48e4b67486c19046cea477ccc
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2023-10-24 00:11:41 +0530

    Updates

--- Core-Java/Strivers/Arrays/RotateArrByKSpace.java
+++ Core-Java/Strivers/Arrays/RotateArrByKSpace.java
@@ -10,18 +10,19 @@ public class RotateArrByKSpace{
     }
 
     public static void rotate(int[] nums, int k) {
-        k %= nums.length;
+        k %= nums.length; // Ensure k is within array bounds
         int n = nums.length;
-        reverseNum(nums,0,n-1);
-        reverseNum(nums,0,k-1);
-        reverseNum(nums,k,n-1);
+        reverseNum(nums,0,n-1); // Reverse entire array
+        reverseNum(nums,0,k-1); // Reverse first k elements
+        reverseNum(nums,k,n-1); // Reverse remaining elements
         System.out.println(Arrays.toString(nums));
     }
     public static void reverseNum(int[] nums, int start, int end) {
         while(start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+            // int temp = nums[start];
+            // nums[start] = nums[end];
+            // nums[end] = temp;
+            nums[start]=nums[start]+nums[end]-(nums[end]=nums[start]);
             start++;
             end--;
         }
