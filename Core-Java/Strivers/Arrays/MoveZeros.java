a836bec806bcdd1ab3ebd305c5357d50dbe6460d
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2023-10-25 01:14:28 +0530

    updates

--- Core-Java/Strivers/Arrays/MoveZeros.java
+++ Core-Java/Strivers/Arrays/MoveZeros.java
@@ -5,29 +5,83 @@ import java.util.*;
 public class MoveZeros {
     public static void main(String[] args) {
         int[] arr = {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
-        sortArr(arr);
+        moveZeros_BF(arr);
+        moveZeroes(arr);
+        int[] arrZero = moveZeros_OP(arr);
+        System.out.println(Arrays.toString(arrZero));
     }
 
-    private static void sortArr(int[] arr) {
-        List<Integer > temp1 = new ArrayList<>();
-        List<Integer > temp2 = new ArrayList<>();
+
+    private static void moveZeros_BF(int[] arr) {
+        List<Integer > temp = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            if (!(arr[i] == 0)) {
-                temp1.add(arr[i]);
-            } else {
-                temp2.add(arr[i]);
+            if (arr[i] != 0) {
+                temp.add(arr[i]);
             }
         }
-        for (int i = 0; i <= arr.length; i++) {
-            if(temp1.get(i) > 0){
-                arr[i] = temp1.get(i);
-            }else{
-                arr[i] = temp2.get(i);
-            }
+        for (int i = 0; i < temp.size(); i++) {
+            arr[i] = temp.get(i);
         }
-        System.out.println(temp1);
-        System.out.println(temp2);
+        for (int i = temp.size(); i < arr.length; i++) {
+            arr[i] = 0;
+        }
         System.out.println(Arrays.toString(arr));
     }
 
+    public static int[] moveZeros_OP(int[] arr) {
+        int n = arr.length;
+        int j = -1; //place the pointer j:
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        //no non-zero elements:
+        if (j == -1) return arr;
+
+        //Move the pointers i and j and swap accordingly:
+        for (int i = j + 1; i < n; i++) {
+            if (arr[i] != 0) { //swap arr[i] & arr[j]:
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                j++;
+            }
+        }
+        return arr;
+    }
+
+    public static void moveZeroes(int[] arr) {
+        int nonZeroIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                if (i != nonZeroIndex) { 
+                    int temp = arr[nonZeroIndex];
+                    arr[nonZeroIndex] = arr[i];
+                    arr[i] = temp;
+                }
+                nonZeroIndex++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void moveZeroes1(int[] nums) {
+        int i = 0;
+        for (int num : nums)
+        {
+            if (num != 0)
+            {
+                nums[i] = num;
+                i++;
+            }
+        }
+        while (i <= nums.length - 1)
+        {
+            nums[i] = 0;
+            i++;
+        }
+
+    }
 }
