fb6c891d00cf0c17fc340ffc941250729fd6f01e
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2023-11-18 01:47:11 +0530

    updates

---
+++ Core-Java/Zoho/Round2/Strings/DistinctSubString.java
@@ -0,0 +1,54 @@
+package Zoho.Round2.Strings;
+
+public class DistinctSubString {
+    public static void main(String[] args) {
+        String s1 = "abcda";
+        System.out.println(countGoodSubstrings(s1)); // Output: 1
+
+        String s2 = "aababcabc";
+        System.out.println(countGoodSubstrings(s2)); // Output: 4
+    }
+
+    public static int countGoodSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i <= s.length() - 3; i++) {
+            if (isGoodSubstring(s, i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isGoodSubstring(String s, int index) {
+        // Check if the substring has distinct characters
+        return s.charAt(index) != s.charAt(index + 1)
+                && s.charAt(index + 1) != s.charAt(index + 2)
+                && s.charAt(index) != s.charAt(index + 2);
+    }
+}
+
+/*
+Substrings of Size Three with Distinct Characters
+
+A string is good if there are no repeated characters.
+
+Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+
+Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+
+A substring is a contiguous sequence of characters in a string.
+
+
+
+Example 1:
+Input: s = "xyzzaz"
+Output: 1
+Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz". 
+The only good substring of length 3 is "xyz".
+
+Example 2:
+Input: s = "aababcabc"
+Output: 4
+Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+The good substrings are "abc", "bca", "cab", and "abc".
+ */
