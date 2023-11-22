ea8383dbf8372d72629ff3f4ad13b2c9cf7e226a
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2023-11-22 10:05:41 +0530

    updates

--- Core-Java/Zoho/Round2/Strings/ReplaceConsonantsWithNearestVowels.java
+++ Core-Java/Zoho/Round2/Strings/ReplaceConsonantsWithNearestVowels.java
@@ -2,26 +2,18 @@ package Zoho.Round2.Strings;
 
 public class ReplaceConsonantsWithNearestVowels {
     public static void main(String[] args) {
-        String inputString = "geeksforgeeks";
-        String result = replaceConsonantsWithNearestVowels(inputString);
+        String inputString = "saranya";
+        String result = replacingConsonants(inputString);
         System.out.println(result);
     }
 
-    public static String replaceConsonantsWithNearestVowels(String input) {
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        char[] chars = input.toCharArray();
-
-        for (int i = 0; i < chars.length; i++) {
-            if (Character.isLetter(chars[i]) && !isVowel(chars[i])) {
-                int index = (chars[i] - 'a') % 5;
-                chars[i] = vowels[index];
-            }
+    static String replacingConsonants(String s){
+        String str = "aaaeeeeiiiiioooooouuuuuuu";
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            int index = ch[i]-'a'; 
+            ch[i] = str.charAt(index);
         }
-
-        return new String(chars);
-    }
-
-    public static boolean isVowel(char c) {
-        return "aeiouAEIOU".indexOf(c) != -1;
+        return new String(ch);
     }
 }
