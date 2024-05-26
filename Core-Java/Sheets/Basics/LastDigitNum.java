de4ac880ce53e9846bf1f671604f0bda6d423f22
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2024-05-27 00:28:46 +0530

    updates

---
+++ Core-Java/Sheets/Basics/LastDigitNum.java
@@ -0,0 +1,5 @@
+package Sheets.Basics;
+
+public class LastDigitNum {
+    
+}
--- Core-Java/Zoho/Round2/Maths/FactorialWithoutMultiplication.java
+++ Core-Java/Zoho/Round2/Maths/FactorialWithoutMultiplication.java
@@ -1,17 +1,11 @@
 package Zoho.Round2.Maths;
 
 public class FactorialWithoutMultiplication {
-	// Function to calculate factorial of the number
-	// without using multiplication operator
 	public static int factorialWithoutMul(int N){
-		// variable to store the final factorial
 		int ans = N;
 
-		// Outer loop
 		for (int i = N - 1; i > 0; i--) {
 			int sum = 0;
-
-			// Inner loop
 			for (int j = 0; j < i; j++)
 				sum += ans;
 			ans = sum;
